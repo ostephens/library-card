2303b30b324497bd6c2547511297464649472441
Owen Stephens <owen@ostephens.com>
2011-11-18 20:46:38 +0000

    adding in another line ...

--- libcard.rb
+++ libcard.rb
@@ -5,7 +5,7 @@ require './lib/account'
 
 warks = Vubis.new('warks','https://library.warwickshire.gov.uk/vs/Pa.csp?OpacLanguage=eng&Profile=Default')
 #damyanti = Account.new('4336855X','18011976','patel.damyanti@gmail.com',warks)
-#owen = Account.new('4356489X','19041972','owen.patel@gmail.com',warks)
+owen = Account.new('4356489X','19041972','owen.patel@gmail.com',warks)
 
 get '/go-go-owen-renew' do
 #    owen.getCurrentloans
