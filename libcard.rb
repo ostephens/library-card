bb5cc24a27a916c78ae7c11c93a5bbed0fcd3c46
Owen Stephens <owen@ostephens.com>
2011-11-18 20:06:44 +0000

    commented more out to see if I can get it working on heroku

--- libcard.rb
+++ libcard.rb
@@ -3,9 +3,9 @@ require 'sinatra'
 require 'lib/librarysystems'
 require 'lib/account'
 
-warks = Vubis.new('warks','https://library.warwickshire.gov.uk/vs/Pa.csp?OpacLanguage=eng&Profile=Default')
-damyanti = Account.new('4336855X','18011976','patel.damyanti@gmail.com',warks)
-owen = Account.new('4356489X','19041972','owen.patel@gmail.com',warks)
+#warks = Vubis.new('warks','https://library.warwickshire.gov.uk/vs/Pa.csp?OpacLanguage=eng&Profile=Default')
+#damyanti = Account.new('4336855X','18011976','patel.damyanti@gmail.com',warks)
+#owen = Account.new('4356489X','19041972','owen.patel@gmail.com',warks)
 
 get '/go-go-owen-renew' do
 #    owen.getCurrentloans
