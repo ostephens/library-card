c3a7c04dd28ad8d5d83016cce80ac05a286192ab
Owen Stephens <owen@ostephens.com>
2011-11-18 17:20:51 +0000

    removed all calls to check working

--- libcard.rb
+++ libcard.rb
@@ -8,8 +8,9 @@ damyanti = Account.new('4336855X','18011976','patel.damyanti@gmail.com',warks)
 owen = Account.new('4356489X','19041972','owen.patel@gmail.com',warks)
 
 get '/go-go-owen-renew' do
-    owen.getCurrentloans
+#    owen.getCurrentloans
 #    owen.renewLoans
 #    owen.getCurrentloans
-    "#{owen.currentloans.to_s}"
+#    "#{owen.currentloans.to_s}"
+    "done"
 end
