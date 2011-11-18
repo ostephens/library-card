f768944f4304c2abcae41f0991c66fbf08a34e27
Owen Stephens <owen@ostephens.com>
2011-11-18 20:45:15 +0000

    added in new library system object creation

--- libcard.rb
+++ libcard.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require './lib/librarysystems'
 require './lib/account'
 
-#warks = Vubis.new('warks','https://library.warwickshire.gov.uk/vs/Pa.csp?OpacLanguage=eng&Profile=Default')
+warks = Vubis.new('warks','https://library.warwickshire.gov.uk/vs/Pa.csp?OpacLanguage=eng&Profile=Default')
 #damyanti = Account.new('4336855X','18011976','patel.damyanti@gmail.com',warks)
 #owen = Account.new('4356489X','19041972','owen.patel@gmail.com',warks)
 
