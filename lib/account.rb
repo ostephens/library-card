5a3c939a0a7080b6ada500e51d7b9a19f560fe2d
Owen Stephens <owen@ostephens.com>
2011-11-18 20:34:27 +0000

    changed relative path on require statemets

--- lib/account.rb
+++ lib/account.rb
@@ -3,8 +3,8 @@ require 'mechanize'
 require 'nokogiri'
 require 'date'
 require 'pony'
-require 'lib/loanitem'
-require 'lib/loanlist'
+require './lib/loanitem'
+require './lib/loanlist'
 
 class Account
     def initialize(barcode, pin, email, libsys)
