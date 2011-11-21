82307dc621b3c0acbb9e0d26db552fa5f127e2d8
Owen Stephens <owen@ostephens.com>
2011-11-21 10:52:38 +0000

    Trying templates

--- libcard.rb
+++ libcard.rb
@@ -10,7 +10,7 @@ owen = Account.new('4356489X','19041972','owen.patel@gmail.com',warks)
 get '/go-go-owen-renew' do
     owen.getLoans
     owen.renewLoans
-    output = "<%= #{owen.currentloans.printLoanlist} >"
+    output = "<%= #{owen.currentloans.printLoanlist} %>"
     erb output
 end
 
