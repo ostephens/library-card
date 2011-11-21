cf97eddb1b0837b5d05123e783008249280edbde
Owen Stephens <owen@ostephens.com>
2011-11-21 10:22:29 +0000

    Trying to improve output format

--- libcard.rb
+++ libcard.rb
@@ -10,7 +10,8 @@ owen = Account.new('4356489X','19041972','owen.patel@gmail.com',warks)
 get '/go-go-owen-renew' do
     owen.getLoans
     owen.renewLoans
-    "#{owen.currentloans.printLoanlist}"
+    output = owen.currentloans.printLoanlist
+    "#{output}"
 end
 
 #get '/go-go-damyanti-renew' do
