da1bce3d50de5ebc6b6fc0460631cd41d20da2a4
Owen Stephens <owen@ostephens.com>
2011-11-21 10:20:43 +0000

    call the correct print function

--- libcard.rb
+++ libcard.rb
@@ -10,7 +10,7 @@ owen = Account.new('4356489X','19041972','owen.patel@gmail.com',warks)
 get '/go-go-owen-renew' do
     owen.getLoans
     owen.renewLoans
-    "#{owen.currentloans.to_html}"
+    "#{owen.currentloans.printLoanlist}"
 end
 
 #get '/go-go-damyanti-renew' do
