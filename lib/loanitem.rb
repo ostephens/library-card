b7683a67bd05f9400dad2895776224ec427de271
Owen Stephens <owen@ostephens.com>
2011-11-21 21:57:43 +0000

    Now checks if items are renewable or not

--- lib/loanitem.rb
+++ lib/loanitem.rb
@@ -1,20 +1,21 @@
 class Loanitem
-    def initialize(id, title, loandate, duedate, renewals)
+    def initialize(id, title, loandate, duedate, renewals, renewable)
         @id = id
         @title = title
         @loandate = loandate
         @duedate = duedate
         @renewals = renewals
+        @renewable = renewable
     end
     
     attr_reader :id, :title, :loandate, :duedate, :renewals, :renewable
 
     def to_s
-        "ID: #{@id} Title: #{@title}  #{@authordesc}     Loan date: #{@loandate}   Due date: #{@duedate}   Renewals: #{@renewals}   Renewable? :#{renewable}\n"
+        "ID: #{@id} Title: #{@title}  #{@authordesc}     Loan date: #{@loandate}   Due date: #{@duedate}   Renewals: #{@renewals}   Renewable? #{renewable}\n"
     end
     
     def printLoanitem
-        printed = "ID: #{@id} Title: #{@title}  #{@authordesc}     Loan date: #{@loandate}   Due date: #{@duedate}   Renewals: #{@renewals}   Renewable? :#{renewable}<br />"
+        printed = "ID: #{@id} Title: #{@title}  #{@authordesc}     Loan date: #{@loandate}   Due date: #{@duedate}   Renewals: #{@renewals}   Renewable? #{renewable}<br />"
     end
     
     def printplainLoanitem
